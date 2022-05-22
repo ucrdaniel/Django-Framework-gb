7cc172bc893237a33162e1e0810d4fc17cb47f82
Daniil <89664655+ucrdaniel@users.noreply.github.com>
2022-05-22 17:07:54 -0300

    Add files via upload

--- basketapp/models.py
+++ basketapp/models.py
@@ -20,6 +20,10 @@ class Basket(models.Model):
     def __str__(self):
         return f'{self.user.username} - {self.product.name} - {self.quantity} - {self.add_datetime}'
 
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.filter(pk=pk).first()
+
     @property
     def product_cost(self):
         return self.product.price * self.quantity
